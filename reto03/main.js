8cfa784b359573280f99c1f3efa78775a262ccd1
Aima <aimaravillega@hotmail.com>
2023-08-09 15:04:57 -0300

    subiendo el reto03, sus inicios

---
+++ reto03/main.js
@@ -0,0 +1,22 @@
+/*alert ("Jony, no llegue y lo hice cartonera wifi style, te debo el premium para la proxima!")
+*/
+
+let titulo = document.getElementById ("titulo")
+let nombre = document.getElementById ("nombre")
+let btn = document.getElementById ("btn")
+let check = document.getElementById ("tarea")
+let label = document.getElementById ("tarea1")
+
+btn.addEventListener ("click",() => {
+    let tipo = nombre.value
+    titulo.innerHTML = tipo + " tus tareas del día son: "
+})
+
+check.addEventListener ("click", () => {
+    if (check.checked) {
+        label.style.textDecoration = "line-through";
+        alert ("Completaste la tarea: " + label.textContent)
+    } else {
+        label.style.textDecoration = "none";
+    }
+});
